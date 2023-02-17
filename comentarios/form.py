d72756c06303dd784d03fd17cee030fcc878eacf
Leandro de Jesus Santos <jstleandro@gmail.com>
2023-02-17 01:36:16 -0300

    recapcha

--- comentarios/form.py
+++ comentarios/form.py
@@ -1,9 +1,23 @@
 from django import forms
 from .models import Comentario
+import requests
 
 
 class ComentarioForm(forms.ModelForm):
     def clean(self):
+        captcha_resp = self.data.get('g-recaptcha-response')
+        post_data = {
+            'secret': '6Lcrj4skAAAAAJwmGopx9Kehjr_PypTf6VurP3Qn', 
+            'response': captcha_resp
+        }
+        captcha_req = requests.post(
+            'https://www.google.com/recaptcha/api/siteverify', 
+            data=post_data
+        )
+        captcha_solved = captcha_req.json()
+        if not captcha_solved.get('success'):
+            self.add_error('comentario', 'reCaptcha inválido.')
+        
         data = self.cleaned_data
         NOME = data.get('nome')
         nome_len = len(NOME)
